2f607b6f36500cf112f074aa1d4d094737e688ba
¨Mario Castro¨ <mnc634@utexas.edu>
2021-02-15 16:46:18 -0600

    added port connections to server js, missing html/api routes

--- Develop/server.js
+++ Develop/server.js
@@ -4,3 +4,13 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// require("./")
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`);
+});
