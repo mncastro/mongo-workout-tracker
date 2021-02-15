b92aa053a6fbfcbdef18973fea09ac751f14ad95
¨Mario Castro¨ <mnc634@utexas.edu>
2021-02-15 16:51:54 -0600

    added routes folders and files and added connections to server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,8 @@
+// Libraries and dependencies variables
 const express = require("express");
 const mongoose = require("mongoose");
 
+// Local port Express connection
 const PORT = process.env.PORT || 3000;
 const app = express();
 
@@ -8,7 +10,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// require("./")
+// HTML and API routes!!
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
+
+//MongoDB and mongoose database connections
+// Server installation and display port
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 app.listen(PORT, () => {
