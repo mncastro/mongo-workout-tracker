46d443f4962c690fecf080df5113bfb23449c0d6
¨Mario Castro¨ <mnc634@utexas.edu>
2021-02-15 18:24:14 -0600

    added html route paths have not checked them

--- Develop/routes/html-routes.js
+++ Develop/routes/html-routes.js
@@ -1,4 +1,13 @@
+// Variables for static route paths
 const path = require("path");
-const root = path.join(_dirname, "../public");
+const root = path.join(__dirname, "../public");
 
-module.expor
+module.exports = function (app) {
+    app.get("/exercise", (req, res) => {
+        res.sendFile(root + "/excercise.html");
+    });
+
+    app.get("/stats", (req, res) => {
+        res.sendFile(root + "/stats.html");
+    });
+};
